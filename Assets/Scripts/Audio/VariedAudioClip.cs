25dc3142a1a45ab7d85ce9c25d70e6f2b3aad692
Krystal McDonald <krystalmc478@gmail.com>
2022-03-22 23:11:07 +1100

    fixed varied audio clip script

--- Assets/Scripts/Audio/VariedAudioClip.cs
+++ Assets/Scripts/Audio/VariedAudioClip.cs
@@ -1,18 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
-[RequireComponent(typeof(AudioClip))]
-public class KAudioSource : MonoBehaviour {
-    
-    public VariedAudioClip vClip;
-    private AudioSource aus;
-    public void Awake() {aus = GetComponent<AudioSource>();}
-
-    public void PlayRandOneShot() {
-        // Get a random clip
-        int index = Random.Range(0,vClip.audioclips.Length-1);
-        float varience = Random.Range(-vClip.pitchVarience / 2f, vClip.pitchVarience / 2f);
-        aus.pitch = vClip.pitch + varience; 
-        aus.PlayOneShot(vClip.audioclips[index]);
-    }
+[CreateAssetMenu(fileName = "Audio", menuName = "KObjects/KAudioClip", order = 1)]
+public class VariedAudioClip : ScriptableObject {
+    public AudioClip[] audioclips;
+    public float pitchVarience;
+    public float pitch;
 }
